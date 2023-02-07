5f7ae26a8b348601733e2fdba810d03c033ca906
hyejinny97 <yhj970102@gmail.com>
2023-02-07 16:28:18 +0900

    Add files in 백준/Bronze

---
+++ 백준/Bronze/15596.py
@@ -0,0 +1,31 @@
+# https://www.acmicpc.net/problem/15596
+# 문제15596번.정수 N개의 합
+# 시간: 0.1초, 메모리: 512MB
+
+
+
+# 입력
+'''
+1. 정수 n개
+'''
+
+
+
+# 출력
+'''
+1. n개의 합을 구하는 함수를 작성
+'''
+
+
+
+# 코드
+import sys
+
+sys.stdin = open('input_text/15596.txt', 'r', encoding='UTF-8')
+
+def solve(a):
+    return sum(a)
+
+
+
+# 실행 결과: 성공(메모리:392944kb, 시간:20ms)
